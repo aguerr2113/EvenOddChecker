9c81cd0343d1fcca90d0b383dca256826db0df9d
Art Guerrero <artemio.guerreru@g.austincc.edu>
2025-03-15 23:28:29 -0500

    import scanner to take in user input,create a number variable for the function,ask user to input a number,create a scanner object from user input, save the the scanner object to the variable used in the function

--- App.java
+++ App.java
@@ -5,11 +5,14 @@ public class App {
     public static void main(String[] args) throws Exception {
         // create a number variable for the function
         int number;
-        // 
+        // ask user to input a number
         System.out.println("Enter a number,and I will see if it is even or odd: ");
+        // create a scanner object from user input
         Scanner sc = new Scanner(System.in);
-
+        // save the the scanner object to the variable used in the function
         number = sc.nextInt();
+
 
     }
 }
+
